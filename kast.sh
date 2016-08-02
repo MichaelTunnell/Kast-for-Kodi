ac21970693f5df0238d08d98d2553428173ba67e
Michael Tunnell <m@michaeltunnell.com>
2016-08-02 16:29:32 -0500

    edited variable names to include kast

--- kast.sh
+++ kast.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#Beamable Variables
-beamurl=$2
+#Kast Variables
+kasturl=$2
 kodiport="8080" #kodi webserver port (default = 80)
 kodiuser="kodi" #kodi webserver user (default = kodi)
 kodipass="kodi" #kodi webserver password (default = kodi)
@@ -13,7 +13,7 @@ kodiipfive=""
 
 if [ -z "$1" ];
 then
-OUTPUT=$(zenity --forms --title="Beamable" --text="" --separator=";" --add-entry="Device ID" --add-entry="URL / Path")
+OUTPUT=$(zenity --forms --title="Kast for Kodi" --text="" --separator=";" --add-entry="Device ID" --add-entry="URL / Path")
 fi
 
 zenid=$(awk -F";" '{print $1}' <<<$OUTPUT)
@@ -26,7 +26,7 @@ fi
 
 if [ -n "$zenpath" ];
 then
-beamurl=${zenpath}
+kasturl=${zenpath}
 fi
 
 if [[ "$1" == '1' ]]
@@ -47,124 +47,124 @@ then
 elif [[ "$1" == *http* ]]
 then
 	kodiip=${kodiipone}
-	beamurl=$1
+	kasturl=$1
 else
 	kodiip=${kodiipone}
 fi
-if [[ "${beamurl}" == *youtube.com* ]]
+if [[ "${kasturl}" == *youtube.com* ]]
 then
-	vid=$( echo "${beamurl}" | tr '?&#' '\n\n' | grep -e '^v=' | cut -d'=' -f2 )
+	vid=$( echo "${kasturl}" | tr '?&#' '\n\n' | grep -e '^v=' | cut -d'=' -f2 )
 	purl='plugin://plugin.video.youtube/?action=play_video&videoid='${vid}
-elif [[ "${beamurl}" == *youtu.be* ]]
+elif [[ "${kasturl}" == *youtu.be* ]]
 then
-	vid=$( echo "${beamurl}" | tr '?&#' '\n\n' | grep -e '/' | cut -d'/' -f4 )
+	vid=$( echo "${kasturl}" | tr '?&#' '\n\n' | grep -e '/' | cut -d'/' -f4 )
 	purl='plugin://plugin.video.youtube/?action=play_video&videoid='${vid}
-elif [[ "${beamurl}" == *vimeo.com* ]]
+elif [[ "${kasturl}" == *vimeo.com* ]]
 then
-	vid=$( echo "${beamurl}" | tr '?&#' '\n\n' | grep -e '/' | cut -d'/' -f4 )
+	vid=$( echo "${kasturl}" | tr '?&#' '\n\n' | grep -e '/' | cut -d'/' -f4 )
 	purl='plugin://plugin.video.vimeo/?action=play_video&videoid='${vid}
-elif [[ "${beamurl}" == *thedailyshow.com* ]]
+elif [[ "${kasturl}" == *thedailyshow.com* ]]
 then
-	vid=$( echo "${beamurl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
-	vname=$( echo "${beamurl}" | grep -e '/' | cut -d'/' -f5 )
+	vid=$( echo "${kasturl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
+	vname=$( echo "${kasturl}" | grep -e '/' | cut -d'/' -f5 )
 	if [[ "$" == *thedailyshow.com/watch* ]]
 	then
-		vname=$( echo "${beamurl}" | grep -e '/' | cut -d'/' -f6 )
+		vname=$( echo "${kasturl}" | grep -e '/' | cut -d'/' -f6 )
 	fi
 	purl='plugin://plugin.video.the.daily.show/?url='${vid}'&mode=play&name='${vname}
-elif [[ "${beamurl}" == *rt.com* ]]
+elif [[ "${kasturl}" == *rt.com* ]]
 then
-	if [[ "${beamurl}" == *on-air/rt-america-air/* ]]
+	if [[ "${kasturl}" == *on-air/rt-america-air/* ]]
 	then
 		purl='plugin://plugin.video.rt/?url=rtmp%3A%2F%2Frt.fms-04.visionip.tv%2Flive%2Frt-america-live-HD&mode=12'
-	elif [[ "${beamurl}" == *on-air/* ]]
+	elif [[ "${kasturl}" == *on-air/* ]]
 	then
 		purl='plugin://plugin.video.rt/?url=rtmp%3A%2F%2Frt.fms-04.visionip.tv%2Flive%2Frt-global-live-HD&mode=12'
-	elif [[ "${beamurl}" == *arabic*/live* ]]
+	elif [[ "${kasturl}" == *arabic*/live* ]]
 	then
 		purl='plugin://plugin.video.rt/?url=rtmp%3A%2F%2Frt.fms-04.visionip.tv%2Flive%2Frt-rusiyaalyaum-live-HD&mode=12'
-	elif [[ "${beamurl}" == *en_vivo* ]]
+	elif [[ "${kasturl}" == *en_vivo* ]]
 	then
 		purl='plugin://plugin.video.rt/?url=rtmp%3A%2F%2Frt.fms-04.visionip.tv%2Flive%2Frt-enespanol-live-HD&mode=12'
 	else
-		vid=$( echo "${beamurl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
-		vname=$( echo "${beamurl}" | grep -e '/' | cut -d'/' -f6 )
+		vid=$( echo "${kasturl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
+		vname=$( echo "${kasturl}" | grep -e '/' | cut -d'/' -f6 )
 		purl='plugin://plugin.video.rt/?url=plugin%3A%2F%2Fplugin.video.rt%2F%3Furl%3D'${vid}'%26name%3D'${vname}'%26mode%3D19&mode=12'
 	fi
-elif [[ "${beamurl}" == *colbertnation.com* ]]
+elif [[ "${kasturl}" == *colbertnation.com* ]]
 then
-	vid=$( echo "${beamurl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
-	vname=$( echo "${beamurl}" | grep -e '/' | cut -d'/' -f5 )
+	vid=$( echo "${kasturl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
+	vname=$( echo "${kasturl}" | grep -e '/' | cut -d'/' -f5 )
 	purl='plugin://plugin.video.the.colbert.report/?url='${vid}'&mode=play&name='${vname}
-elif [[ "${beamurl}" == *break.com* ]]
+elif [[ "${kasturl}" == *break.com* ]]
 then
-	vid=$( echo "${beamurl}" | awk -F"-" '{print ($(NF))}' )
+	vid=$( echo "${kasturl}" | awk -F"-" '{print ($(NF))}' )
 	purl='plugin://plugin.video.break_com/?url='${vid}'&mode=playVideo'
-elif [[ "${beamurl}" == *time.com* ]]
+elif [[ "${kasturl}" == *time.com* ]]
 then
-	vid=$( echo "${beamurl}" | tr '_' '\n\n' | grep -e '/' | cut -d'/' -f7 | cut -d',' -f3 )
+	vid=$( echo "${kasturl}" | tr '_' '\n\n' | grep -e '/' | cut -d'/' -f7 | cut -d',' -f3 )
 	purl='plugin://plugin.video.time_com/?url='${vid}'&mode=playBrightCoveStream'
-elif [[ "${beamurl}" == *collegehumor.com* ]]
+elif [[ "${kasturl}" == *collegehumor.com* ]]
 then
-	vid=$( echo "${beamurl}" | cut -c29- | perl -MURI::Escape -ne 'print uri_escape($_)' )
+	vid=$( echo "${kasturl}" | cut -c29- | perl -MURI::Escape -ne 'print uri_escape($_)' )
 	purl='plugin://plugin.video.collegehumor/watch/'${vid}
-elif [[ "${beamurl}" == *twitch.tv* ]]
+elif [[ "${kasturl}" == *twitch.tv* ]]
 then
-	vid=$( echo "${beamurl}" | cut -c22- )
+	vid=$( echo "${kasturl}" | cut -c22- )
 	purl='plugin://plugin.video.twitch/playLive/'${vid}'/'
-elif [[ "${beamurl}" == *dailymotion.com* ]]
+elif [[ "${kasturl}" == *dailymotion.com* ]]
 then
-	vid=$( echo "${beamurl}" | cut -c34-41 )
+	vid=$( echo "${kasturl}" | cut -c34-41 )
 	purl='plugin://plugin.video.dailymotion_com/?url='${vid}'&mode=playVideo'
-elif [[ "${beamurl}" == *mixcloud.com* ]]
+elif [[ "${kasturl}" == *mixcloud.com* ]]
 then
-	vid=$( echo "${beamurl}" | cut -c24- | perl -MURI::Escape -ne 'print uri_escape($_)' )
+	vid=$( echo "${kasturl}" | cut -c24- | perl -MURI::Escape -ne 'print uri_escape($_)' )
 	purl='plugin://plugin.audio.mixcloud/?mode=40&key='${vid}
-elif [[ "${beamurl}" == *theonion.com* ]]
+elif [[ "${kasturl}" == *theonion.com* ]]
 then
-	vid=$( echo "${beamurl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
+	vid=$( echo "${kasturl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
 	purl='plugin://plugin.video.theonion_com/?url='${vid}'&mode=playVideo'
-elif [[ "${beamurl}" == *cbsnews.com* ]]
+elif [[ "${kasturl}" == *cbsnews.com* ]]
 then
-	vid=$( echo "${beamurl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
+	vid=$( echo "${kasturl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
 	purl='plugin://plugin.video.cbsnews_com/?url='${vid}'&mode=playVideo'
-elif [[ "${beamurl}" == *ign.com* ]]
+elif [[ "${kasturl}" == *ign.com* ]]
 then
-	vid=$( echo "${beamurl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
+	vid=$( echo "${kasturl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
 	purl='plugin://plugin.video.ign_com/?url='${vid}'&mode=playVideo'
-elif [[ "${beamurl}" == *howstuffworks.com* ]]
+elif [[ "${kasturl}" == *howstuffworks.com* ]]
 then
-	vid=$( echo "${beamurl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
+	vid=$( echo "${kasturl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
 	purl='plugin://plugin.video.howstuffworks_com/?url='${vid}'&mode=playVideo'
-elif [[ "${beamurl}" == *ustream.tv* ]]
+elif [[ "${kasturl}" == *ustream.tv* ]]
 then
-	vid=$( echo "${beamurl}" | tr '?&#' '\n\n' | grep -e 'socialstream/' | cut -d'/' -f5 )
+	vid=$( echo "${kasturl}" | tr '?&#' '\n\n' | grep -e 'socialstream/' | cut -d'/' -f5 )
 	purl='http://iphone-streaming.ustream.tv/ustreamVideo/'${vid}'/streams/live/playlist.m3u8'
-elif [[ "${beamurl}" == *ebaumsworld.com* ]]
+elif [[ "${kasturl}" == *ebaumsworld.com* ]]
 then
-	vid=$( echo "${beamurl}" | grep -e '/' | cut -d'/' -f6 )
+	vid=$( echo "${kasturl}" | grep -e '/' | cut -d'/' -f6 )
 	purl='plugin://plugin.video.ebaumsworld_com/?url='${vid}'&mode=playVideo'
-elif [[ "${beamurl}" == *roosterteeth.com* ]]
+elif [[ "${kasturl}" == *roosterteeth.com* ]]
 then
-	vid=$( echo "${beamurl}" | tr '?&#' '\n\n' | grep -e '^id=' | cut -d'=' -f2 )
+	vid=$( echo "${kasturl}" | tr '?&#' '\n\n' | grep -e '^id=' | cut -d'=' -f2 )
 	purl='plugin://plugin.video.roosterteeth/?url='${vid}'&mode=3&name=Filename+Unvailable'
-elif [[ "${beamurl}" == *aljazeera.com* ]]
+elif [[ "${kasturl}" == *aljazeera.com* ]]
 then
 	purl='plugin://plugin.video.aljazeera/live/'
-elif [[ "${beamurl}" == *magnet:* ]]
+elif [[ "${kasturl}" == *magnet:* ]]
 then
-	vid=$( echo "${beamurl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
+	vid=$( echo "${kasturl}" | perl -MURI::Escape -ne 'print uri_escape($_)' )
 	purl='plugin://plugin.video.xbmctorrent/play/'${vid}
-elif [[ "${beamurl}" == *file://* ]]
+elif [[ "${kasturl}" == *file://* ]]
 then
-	vid=$( echo "${beamurl}" | awk -F"file://$HOME" '{print ($(NF))}' )
+	vid=$( echo "${kasturl}" | awk -F"file://$HOME" '{print ($(NF))}' )
 	purl=${smbpath}${vid}
-elif [[ "${beamurl}" == */home/* ]]
+elif [[ "${kasturl}" == */home/* ]]
 then
-	vid=$( echo "${beamurl}" | awk -F"$HOME" '{print ($(NF))}' )
+	vid=$( echo "${kasturl}" | awk -F"$HOME" '{print ($(NF))}' )
 	purl=${smbpath}${vid}
 else
-	purl=${beamurl}
+	purl=${kasturl}
 fi
 
 payload='{"jsonrpc": "2.0", "method": "Player.Open", "params":{ "item": {"file" : "'${purl}'" }}, "id" : 1}'
